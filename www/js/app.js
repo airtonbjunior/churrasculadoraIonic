a9152c0dd6619ff94aa231c1efc8a2f59398bb32
Airton Jr <airtonbjunior@gmail.com>
2016-05-10 20:14:36 -0300

    Message to the users on console

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,6 @@
-// Ionic Starter App
+console.log("Minha aplicação pra teste. Quer entender um pouco melhor da bagunça dessa App? Dá uma olhadinha em Resources/localStorage. :)")
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
+
 var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
